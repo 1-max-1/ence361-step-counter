4e11cbf698dd63d813230eb3aef44448506cd973
Maxh <44454544+1-max-1@users.noreply.github.com>
2025-03-07 14:54:43 +1300

    Added joystick polling task

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -1,15 +1,28 @@
 #include "gpio.h"
 #include "rgb.h"
 #include "buttons.h"
+#include "adc.h"
 
 #define TICK_FREQUENCY_HZ 1000
 #define HZ_TO_TICKS(FREQUENCY_HZ) (TICK_FREQUENCY_HZ/FREQUENCY_HZ)
 
 #define BLINKY_TASK_PERIOD_TICKS (TICK_FREQUENCY_HZ / 2)
 #define BUTTON_TASK_PERIOD_TICKS (TICK_FREQUENCY_HZ / 100)
+#define JOYSTICK_TASK_PERIOD_TICKS (TICK_FREQUENCY_HZ / 150)
 
 static uint32_t blinkyTaskNextRun = 0;
 static uint32_t buttonTaskNextRun = 0;
+static uint32_t joystickTaskNextRun = 0;
+
+static uint16_t raw_adc[2];
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+
+}
+
+void joystick_task_execute() {
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)raw_adc, 2);
+}
 
 void blinky_task_execute() {
 	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
@@ -42,6 +55,11 @@ void app_main(void) {
 	if (ticks > buttonTaskNextRun) {
 		button_task_execute();
 		buttonTaskNextRun += BUTTON_TASK_PERIOD_TICKS;
+	}
+
+	if (ticks > joystickTaskNextRun) {
+		joystick_task_execute();
+		joystickTaskNextRun += JOYSTICK_TASK_PERIOD_TICKS;
 	}
 }
 
